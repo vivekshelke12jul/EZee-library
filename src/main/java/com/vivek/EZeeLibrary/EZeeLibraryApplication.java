4b75ca6478556bbdead05c7f212fefe000740b1d
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-03-26 22:08:25 +0530

    first commit

---
+++ src/main/java/com/vivek/EZeeLibrary/EZeeLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.EZeeLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EZeeLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EZeeLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivek/EZeeLibrary/EZeeLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.EZeeLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EZeeLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
